1ac8960981f56a840cf771870ae588d014cb1ef4
Victor Mateus <victormdev@gmail.com>
2025-02-03 14:57:57 -0300

    função adicionar amigos implementada

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
+
+function adicionarAmigo(){
+    let nome = document.getElementById('amigo').value.trim();
+
+    if(nome == "") {
+        alert("Por favor, insira um nome.");
+    }
+    else {
+        if(amigos.includes(nome)) {
+            alert("Por favor, não repetir um nome.");
+            document.getElementById('amigo').value = '';
+        }
+        else {
+            amigos.push(nome);
+            document.getElementById('amigo').value = '';
+        }
+    }
+
+    console.log(amigos);
+}
